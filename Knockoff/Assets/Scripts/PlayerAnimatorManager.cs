3feb8ee956a98302baa8fdc40e2621b0ee6687fa
mchami7 <84527058+mchami7@users.noreply.github.com>
2023-04-17 19:44:18 +0100

    player setup with basic movements and animations

---
+++ Knockoff/Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KnockOff.Player
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region Private Fields 
+
+        private Animator anim;
+
+        [SerializeField] private float directionDampTime = 0.25f;
+
+        #endregion
+
+
+        #region Monobehaviour Callbacks
+
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+
+            if (!anim)
+            {
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+            }
+        }
+
+
+        private void Update()
+        {
+            if (!anim)
+                return;
+
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetTrigger("Jump");
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (v < 0)
+                v = 0;
+
+            anim.SetFloat("Speed", h * h + v * v);
+            anim.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+
+        }
+
+        #endregion
+    }
+
+}
