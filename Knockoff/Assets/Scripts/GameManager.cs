9262f7a2284da7619978d1fb26bf208be6dbdb06
mchami7 <84527058+mchami7@users.noreply.github.com>
2023-04-17 16:51:24 +0100

    gamemanager and levels - users in sync in  rooms

--- Knockoff/Assets/Scripts/GameManager.cs
+++ Knockoff/Assets/Scripts/GameManager.cs
@@ -18,6 +18,32 @@ namespace KnockOff.Game
             SceneManager.LoadScene(0);  //launcher scene
         }
 
+        public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", otherPlayer.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+
+
         #endregion
 
         #region Public Methods
@@ -28,6 +54,23 @@ namespace KnockOff.Game
         public void LeaveRoom()
         {
             PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        public void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+                return;
+            }
+
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);   // we don't use Unity directly, because we want to rely on Photon to load this level on all connected clients in the room,
+                                                                                            // since we've enabled PhotonNetwork.AutomaticallySyncScene for this Game.
         }
 
         #endregion
