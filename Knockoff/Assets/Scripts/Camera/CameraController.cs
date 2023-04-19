0d1a2e4f5303704d4fe380d9a5a56b9e5a96e90b
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-04-19 17:55:37 +0100

    Removed the TDSShooter script
    Removed the TDSShooter script and placed the contents in CameraController script.

--- Knockoff/Assets/Scripts/Camera/CameraController.cs
+++ Knockoff/Assets/Scripts/Camera/CameraController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using Photon.Pun;
+using KnockOff.Player;
 
 namespace KnockOff.Camera
 {
     public class CameraController : MonoBehaviourPun
     {
         private GameObject virtualCamera;
+        private GameObject aimVirtualCam;
+        [SerializeField] private float normalSensitivity;
+        [SerializeField] private float aimSensitivity;
 
+        private PlayerMovement playerMovement;
+
+        private void Awake()
+        {
+            playerMovement = GetComponent<PlayerMovement>();
+        }
         private void Update()
         {
             // Only execute this code for the local player
@@ -17,14 +27,41 @@ namespace KnockOff.Camera
             {
                 return;
             }
-
+            PhotonView playerPhotonView = GetComponent<PhotonView>();
             // Find the CinemachineVirtualCamera object in the scene
+            VirtualCam(playerPhotonView);
+            AimCam(playerPhotonView);
+        }
+
+        private void AimCam(PhotonView playerPhotonView)
+        {
+            if (aimVirtualCam == null)
+            {
+                aimVirtualCam = GameObject.FindGameObjectWithTag("aimCam");
+            }
+            // Set the virtual camera to follow the local player
+            if (playerPhotonView != null)
+            {
+                aimVirtualCam.GetComponent<CinemachineVirtualCamera>().Follow = playerPhotonView.transform.GetChild(0);//camTarget, less expensive than findbytag                                                                                            
+            }
+            if (Input.GetMouseButton(1))
+            {
+                aimVirtualCam.gameObject.SetActive(true);
+                playerMovement.SetSensitivity(aimSensitivity);
+            }
+            else
+            {
+                aimVirtualCam.gameObject.SetActive(false);
+                playerMovement.SetSensitivity(normalSensitivity);
+            }
+        }
+
+        private void VirtualCam(PhotonView playerPhotonView)
+        {
             if (virtualCamera == null)
             {
                 virtualCamera = GameObject.FindGameObjectWithTag("3dCam");
             }
-
-            PhotonView playerPhotonView = GetComponent<PhotonView>();
             // Set the virtual camera to follow the local player
             if (playerPhotonView != null)
             {
