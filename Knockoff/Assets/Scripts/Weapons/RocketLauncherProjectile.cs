d9f9a15f38bf80825993e06fae8e77797645e9e8
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-04-25 17:55:23 +0100

    Needs fixing with fire rotation

--- Knockoff/Assets/Scripts/Weapons/RocketLauncherProjectile.cs
+++ Knockoff/Assets/Scripts/Weapons/RocketLauncherProjectile.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using KnockOff.Player;
 
 public class RocketLauncherProjectile : Gun
 {
     private Rigidbody projectile;
     public Transform SpawnPoint;
+    public Transform camTarget;
+
     [SerializeField]
     float projectileSpeed = 10f;
+
+    private void Start()
+    {
+        camTarget = Utils.FindWithTag(PlayerManager.LocalPlayerInstance.transform, "CamTarget");
+    }
     private void Awake()
     {
         projectile = GetComponentInChildren<Rigidbody>();
+
     }
     public override void Use()
     {
@@ -19,7 +28,8 @@ public class RocketLauncherProjectile : Gun
     }
     void Shoot(Rigidbody _projectile)
     {
-        _projectile = Instantiate(projectile,SpawnPoint.position, transform.rotation);
-        _projectile.velocity = transform.forward * projectileSpeed;
+        SpawnPoint.localRotation = camTarget.localRotation;
+        _projectile = Instantiate(projectile,SpawnPoint.position, SpawnPoint.localRotation);
+        _projectile.velocity = transform.rotation*(Vector3.forward * projectileSpeed);
     }
 }
