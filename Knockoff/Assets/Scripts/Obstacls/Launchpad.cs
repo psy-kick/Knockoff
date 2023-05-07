7011476b5686cda3f6beadb03800310cfc0492bb
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-05-07 13:57:05 +0100

    organized the game scene a bit

--- Knockoff/Assets/Scripts/Obstacls/Launchpad.cs
+++ Knockoff/Assets/Scripts/Obstacls/Launchpad.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Launchpad : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField]private float force = 10f; // the amount of force to apply
+    [SerializeField]private float force = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +16,6 @@ public class Launchpad : MonoBehaviour
         // check if the collision is with a character or other solid object
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("bounce");
             // apply a force in the opposite direction of the collision normal
             Vector3 normal = collision.contacts[0].normal;
             rb.AddForce(-normal * force, ForceMode.Impulse);
