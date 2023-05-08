7baeb86572dcdecf3d3d8e48331dfc87a2cde63f
Suhas Aditya SM <suhasadityasm@gmail.com>
2023-05-08 14:42:36 +0100

    Added the water gun effect

--- Knockoff/Assets/Scripts/Weapons/WaterGun.cs
+++ Knockoff/Assets/Scripts/Weapons/WaterGun.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using Photon.Pun;
 
 public class WaterGun : Gun
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject WaterprojectilePrefab;
+    public Transform SpawnPoint;
+    public LayerMask aimLayerMask;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] float projectileSpeed = 10f;
+
     public override void Use()
     {
-        
+        WaterShoot();
+    }
+
+    void WaterShoot()
+    {
+        Vector3 mouseWorldPos = Vector3.zero;
+        Vector2 screencenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screencenter);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 999f, aimLayerMask))
+        {
+            mouseWorldPos = hitInfo.point;
+            Vector3 aimDir = (mouseWorldPos - SpawnPoint.position).normalized;
+            GameObject _projectile = PhotonNetwork.Instantiate(WaterprojectilePrefab.name, SpawnPoint.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            _projectile.GetComponent<Rigidbody>().velocity = _projectile.transform.forward * projectileSpeed;
+
+            StartCoroutine(WaitForBullet(_projectile));
+        }
+    }
+
+    private IEnumerator WaitForBullet(GameObject p)
+    {
+        yield return new WaitForSeconds(5f);
+        PhotonNetwork.Destroy(p);
     }
 }
